298ebc26f5cf678ede7faef6b97b2285d195f710
onesamblack <onesamblack@gmail.com>
2023-02-18 10:52:36 +0000

    flesh out daemon

--- noidd/noidd.py
+++ noidd/noidd.py
@@ -20,36 +20,63 @@ import warnings
 from functools import wraps
 from typing import TypeVar, Union, AnyStr, Mapping, Iterable, Optional, AsyncGenerator
 
+logging.basicConfig(filename=logname,
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.DEBUG)
 
+logger = logging.getLogger('noidd')
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-d","--daemon", help="run noidd as a daemon", default=True)
 parser.add_argument("-c","--config", help="configuration file (.yml)", default="/etc/noidd/config.yml")
 parser.add_argument("-l","--leveldb", help="filepath of leveldb", default="/etc/noidd/noidd.db")
+parser.add_argument("--logfile", help="logging path of leveldb", default=None)
+parser.add_argument("--recreate", help="recreate the leveldb", action="store_true", default=False)
 args = parser.parse_args()
 
-def load_config(filename:str=args.config):
-    pass
+try:
+    config = yaml.loads(args.config)
+
+except Exception as e:
+    print("couldn't initialize noidd - an exception occurred while loading the config.yml")
+    print(str(e))
+    sys.exit(1)
 
 
-def initialize_db(filename:str=args.leveldb, *args, **kwargs):
-    """
-    initializes a db with noidd information
+logging.basicConfig(filename=f"{config['noidd_root']}/noidd.log" if not args.logfile else args.logfile,
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=logging.DEBUG)
 
-    creates prefixes for all watches
-    sets dates for first watch, last_watch
+logging.info("Starting noidd")
 
-    """
-    pass
+logger = logging.getLogger('noidd')
 
-def load_db(filename:str=args.leveldb) 
-    """
-    gets the noidd db or creates one if it doesn't exist
-    """
-    pass
 
-def check_existing_watches() -> Sequence[str]:
-    """
-    check the noiddb for existing watches
-    """
-    pass
+class Noidd:
+    def __init__(self):
+        self.watchers = []
+        self.db = None
+        self.complete = False
+
+async def initialize():
+    logger.info("initializing")
+    noidd = Noidd()
+    notifiers = []
+    watchers = [] 
+    for w in config["watch"]:
+        n = w["name"]
+        if w["type"] == "dir":
+            watcher = Watcher(name=n,  
+
+    
+
+def main(noidd):
+    
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(initialize())
+    loop.run_forever(main())
